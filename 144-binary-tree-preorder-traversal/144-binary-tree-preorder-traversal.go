c112e2771701a5ac9636b086d9054248e466ca1e
quserinn22 <101107158+quserinn22@users.noreply.github.com>
2022-04-08 19:22:36 +0900

    Time: 2 ms (37.12%), Space: 2.1 MB (41.57%) - LeetHub

---
+++ 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.go
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func preorderTraversal(root *TreeNode) []int {
+    if root == nil {
+        return []int{}
+    }
+    ret := []int{}
+    ret = append(ret, root.Val)
+    if root.Left != nil {
+        ret = append(ret, preorderTraversal(root.Left)...)
+    }
+    
+    if root.Right != nil {
+        ret = append(ret, preorderTraversal(root.Right)...)
+    }
+    
+    return ret
+}
